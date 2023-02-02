e9e9b2277c08292c9613790e221db0adcc143433
Marveeen <jmvroqueee@gmail.com>
2023-02-02 18:03:57 +0800

    added Expenses components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
+import { Expenses } from "./components/Expense";
+
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      date: new Date(2023, 1, 30),
+      title: "Car insurance",
+      amount: 295.64,
+    },
+    {
+      id: "e2",
+      date: new Date(2023, 1, 31),
+      title: "House",
+      amount: 150.64,
+    },
+    {
+      id: "e3",
+      date: new Date(2023, 2, 1),
+      title: "Grocery",
+      amount: 59.75,
+    },
+    {
+      id: "e4",
+      date: new Date(2023, 2, 3),
+      title: "Gas",
+      amount: 20.65,
+    },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
---
+++ src/components/Expense/Expenses.jsx
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+
+import "./styles.css";
+
+function Expenses({ expenses }) {
+  return (
+    <div className="expenses">
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;

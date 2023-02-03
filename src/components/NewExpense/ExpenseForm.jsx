782d85acd8d2400b346baba4597ec5fae39c3fe8
Marveeen <jmvroqueee@gmail.com>
2023-02-03 18:30:20 +0800

    make the form controlled state

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ExpenseForm = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    amount: '',
+    date: ''
+  });
+
+  const onChangeHandler = (e) => {
+    const keyName = e.target.name;
+    const value = e.target.value;
+
+    setFormData((prevState) => {
+      return { ...prevState, [keyName]: value };
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={formData.title}
+            onChange={onChangeHandler}
+            required
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" id="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            min="0.01"
+            step="0.01"
+            value={formData.amount}
+            onChange={onChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" id="title" min="2019-01-01" max="2025-12-31" />
+          <input
+            type="date"
+            name="date"
+            id="date"
+            min="2019-01-01"
+            max="2025-12-31"
+            value={formData.date}
+            onChange={onChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
